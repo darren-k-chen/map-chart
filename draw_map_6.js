ceea889ce29186c8934da42e147018eff05e8c63
K.J. Chen <kjchen@pm.me>
2020-05-03 15:15:04 +0800

    v2005.3.0_commit

---
+++ draw_map_6.js
@@ -0,0 +1,20 @@
+// Author: Darren K.J. Chen
+
+require([
+    "esri/views/MapView",
+	"esri/WebMap"
+], function(MapView, WebMap/*, Map, FeatureLayer*/) {
+
+var client_id = "05928d6e0d184934813e1773d7d44fa5";
+
+var webmap = new WebMap({
+  portalItem: {
+    id: client_id
+  }
+});
+
+var view = new MapView({
+	container: "viewDiv",
+	map: webmap
+});
+});
