82146cc0d2bd6d30ecef0551133073bcbdc53a5f
K.J. Chen <kjchen@pm.me>
2020-05-04 01:58:48 +0800

    v2005.4.3_commit

--- item_id.js
+++ item_id.js
@@ -1,2 +1,2 @@
-// Please replace your item ID with "05928d6e0d184934813e1773d7d44fa5"
+// Please replace "05928d6e0d184934813e1773d7d44fa5" with your item ID
 var item_id = "05928d6e0d184934813e1773d7d44fa5";
